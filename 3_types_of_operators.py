5905fce1912334acc9a859f00fa9cf0f051d4508
batmannishere <pulkit20.pant@gmail.com>
2026-07-01 21:32:24 +0530

    3_types_of_operators

---
+++ 3_types_of_operators.py
@@ -0,0 +1,14 @@
+''' An operator is a symbol that perform a certain poeration btw operands
+1)Arithmetic operators(+,-,*,/,%,**)
+2)Relation operators(==,!=,>,<,>=,<=)
+3)Assignment operators(=,+=,-=,*=,/=,%=,**=)
+4)Logical operators(not,and,or)
+ '''
+a = 5
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) # remainder
+print(a**b) # power
