fe135cdfdf34698d4c18d3979e6e8636de7f56ee
batmannishere <pulkit20.pant@gmail.com>
2026-07-01 20:56:40 +0530

    Added variables and data types

---
+++ variables_and_data_types.py
@@ -0,0 +1,19 @@
+''' A Variables is a name given to a memory location in a program '''
+
+name="Pulkit"
+age = 23
+price = 25.99
+'''how to use a variables'''
+
+print("My name is :",name)
+print("My age is : ",age)
+print("My name is ",name ," and my age is  ",age)
+
+
+''' We use data types to describe the varibles type
+there are five types of data types
+1)Integers-{+ve,-ve,0}{int}{eg-1,2,5}
+2)String-{sentences}{str}{eg-"Pulkit"}
+3)Float-{in decimals}{float}{eg-12.34}
+4)Boolean-{true or false}{bool}
+5)None-{no value}{none}'''
