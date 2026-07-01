c38293d54227b39a9a25b29d3e57a99291d1862e
batmannishere <pulkit20.pant@gmail.com>
2026-07-02 04:43:00 +0530

    Added if-else traffic light program

---
+++ 13_traffic_light.py
@@ -0,0 +1,10 @@
+light=input("what colour can the driver can see in traffic post:")
+
+if(light=="red"):
+    print("stop","dont move")
+elif(light=="yellow"):
+    print("be ready to move")
+elif(light=="green"):
+    print("u can move")
+else:
+    print("traffic light is broken")
