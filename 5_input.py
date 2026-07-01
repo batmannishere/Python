92d7fcc0bdf3faadb51456b4e5a4aa8cd4bdb240
batmannishere <pulkit20.pant@gmail.com>
2026-07-01 22:36:09 +0530

    5_input.py

---
+++ 5_input.py
@@ -0,0 +1,8 @@
+# How to take input in python
+input("Enter your name :")
+# When we take intput in python the input is always in string so we type cast it if we want some value
+a= input ("Enter first number :")
+b= input ("Enter second number :")
+#and to type cast it we use data type outside the input
+a=int(input("Enter first number :"))
+b=int(input("Enter second number :"))
