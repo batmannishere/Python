9cc40086b14c057994386e4cf0ca5a860a4c0de5
batmannishere <pulkit20.pant@gmail.com>
2026-07-01 21:49:22 +0530

    4_type_casting

---
+++ 4_type_casting.py
@@ -0,0 +1,15 @@
+''' 
+CONVERSION- It is done automatically
+CASTING- It is done manually
+'''
+
+a = 5
+b = 6.25
+sum=a+b
+print(sum) #conversion done by system automatically
+c =int("2")# here we type casted a string into a integer
+d = 4
+sum = c+d
+print(sum)
+
+
