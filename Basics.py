551ce0c007569d72425762a0c3976054bd14b1d8
batmannishere <pulkit20.pant@gmail.com>
2026-07-01 14:59:17 +0530

    Initial commit

---
+++ Basics.py
@@ -0,0 +1,9 @@
+#how to print something
+print("Hi my name is pulkit")
+#how to print tow diff senteces in one line(we use comma)
+print("hello","how are u doin")
+#how to print numbers(you dont need inverted commas)
+print(25)
+print(30)
+#u can do maths in round brackets
+print(25+34)
