bbcd4aa81e41d6e2c26f003b22e5c71103f35f80
batmannishere <pulkit20.pant@gmail.com>
2026-07-02 20:06:28 +0530

    python -u

---
+++ 19_tuples.py
@@ -0,0 +1,13 @@
+'''
+TUPLES
+A built in data type that lets us create immutable sequence of value
+SYNTAX
+nameoftuple=()
+TUPLE METHODS
+nameoftuple.count(element): Returns the number of occurrences of a specified element in the tuple. 
+nameoftuple.index(element, start, end): Returns the index of the first occurrence of a specified element; optional start and end parameters allow searching within a specific range.
+len(nameoftuple): Returns the number of elements. 
+min(nameoftuple) and max(nameoftuple): Return the smallest and largest elements, respectively. 
+sorted(nameoftuple): Returns a new list with elements sorted in ascending order (note: it returns a list, not a tuple). 
+nameoftuple.(iterable): Converts an iterable (like a list or string) into a new tuple.
+'''
