f999848d36a36dd5187d790f2003be3ec822eefd
batmannishere <pulkit20.pant@gmail.com>
2026-09-02 01:05:42 +0530

    Add 01_area_rectangle.py

---
+++ projects/01_area_rectangle.py
@@ -0,0 +1,5 @@
+length=float(input("Enter the lenght of a rectangle: "))
+breadth=float(input("Enter the breadth of a rectangle: "))
+area= length*breadth
+print(f"Area of rectangle of length {length} and breadth {breadth} is: {area} ")
+print(type(area))
