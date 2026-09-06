4f890bb175941f6d05a2639adf7709c3fbeaa2e7
batmannishere <pulkit20.pant@gmail.com>
2026-09-06 06:40:32 +0530

    Add 05_hypotenuse.py

---
+++ projects/05_hypotenuse.py
@@ -0,0 +1,5 @@
+import math
+perpendicular=float(input("enter perpendicular of a triangle: "))
+base=float(input("enter base of a triangle: "))
+hypotenuse=math.sqrt(pow(perpendicular,2)+pow(base,2))
+print(f"hypotenuse of a triangle is : {hypotenuse}")
