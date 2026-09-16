c1ce401e8e0adad72534102b508f711342ad741e
batmannishere <pulkit20.pant@gmail.com>
2026-09-16 23:13:06 +0530

    Add 05_super_function.py

---
+++ OOPS/05_super_function.py
@@ -0,0 +1,22 @@
+class Shape():
+    def __init__(self,colour,is_filled):
+     self.colour=colour
+     self.colour=filled
+
+class Circle(Shape):
+    def __init__(self,colour,is_filled,raidus):
+     super().__init__(colour,is_filled)
+     self.raidus=raidus
+
+class Square(Shape):
+    def __init__(self,colour,is_filled,width):
+         super().__init__(colour,is_filled)
+         self.width=width
+
+class Triangle(Shape):
+    def __init__(self,colour,is_filled,weight,height):
+        super().__init__(colour,is_filled)
+        self.width=width
+        self.height=height
+
+
